1a496043ea6be258e474d1d6f85df5474a77e561
Kel Markert <kmarkert@aurelius.socrates.work>
2020-05-07 15:24:41 -0400

    fixing minor syntax errors and print info

--- etl/run_etl.py
+++ etl/run_etl.py
@@ -1,6 +1,6 @@
 import subprocess
 import datetime
-import time
+
 
 script = 'jason.py'
 sensor = 'jason2'
@@ -12,13 +12,13 @@ landFile = '../data/ancillary/land_area.geojson'
 
 start = datetime.datetime.strptime('2008-07-04',"%Y-%m-%d")
 end = datetime.datetime.strptime('2008-12-31',"%Y-%m-%d")
-iter = (end-start).days + 1
+iters = (end-start).days + 1
 
 for t in range(iters):
-    date = startTime + datetime.timedelta(t)
+    date = start + datetime.timedelta(t)
     dateStr = date.strftime('%Y-%m-%d')
-    print(f'{time.now()}:ingesting {dateStr} for {sensor}...')
+    print(f'{datetime.datetime.now()}: ingesting {dateStr} for {sensor}...')
     cmd = f'python {script} etl {sensor} {workingdir} {dbname} --startTime {dateStr} --endTime {dateStr} --username {username} --spatialFilter {landFile} --cleanup'
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     out, err = proc.communicate()
-    print(f'{time.now()}: done ingesting \n')
+    print(f'{datetime.datetime.now()}: done ingesting \n')
