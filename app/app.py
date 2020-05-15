8ec21d81391d800fd3c058356cbd715ef269f15d
kmarkert <kel.markert@gmail.com>
2020-05-15 06:48:59 -0500

    adding 500 error page

--- app/app.py
+++ app/app.py
@@ -24,8 +24,11 @@ def info_page():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 @app.route("/api/", methods=['GET'])
 def get_capabilities():
