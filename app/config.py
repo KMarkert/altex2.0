15f15d0444898bd008ba1f5a70872c862f909065
kmarkert <kel.markert@gmail.com>
2020-11-12 10:26:09 -0600

    updating config parameters for flatfile backend

--- app/config.py
+++ app/config.py
@@ -1,5 +1,1 @@
-DBHOST = '127.0.0.1' # note this is defulat local host
-DBPORT = 5432 # note this is defulat postgres port
-DBNAME = '<dbname>'
-DBUSERNAME = '<username>'
-DBPASSWORD = '<password>'
+DATADIR = "<directory/to/data/>"
